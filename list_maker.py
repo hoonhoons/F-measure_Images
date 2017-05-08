7853577154431ab5358815220ce50c8388298ac8
PARK SEONGHOON <hoonhoons.kr@gmail.com>
2017-05-08 22:00:23 +0900

    add list_maker.py
    list_maker makes lists

---
+++ list_maker.py
@@ -0,0 +1,17 @@
+input_list = open('input_list.txt', 'w')
+output_list = open('output_list.txt', 'w')
+truth_list = open('truth_list.txt', 'w')
+
+input_base = "D:\\\\\\\\Documents\\\\project2_dataset\\\\images\\\\"
+output_base = "D:\\\\\\\\Documents\\\\project2_dataset\\\\results\\\\"
+truth_base = "D:\\\\\\\\Documents\\\\project2_dataset\\\\gt\\\\"
+
+for i in range(301):
+    img = '%03d.jpg\n' % (i)
+    input_list.write(input_base + img)
+    output_list.write(output_base + img)
+    truth_list.write(truth_base + img)
+
+input_list.close()
+output_list.close()
+truth_list.close()
