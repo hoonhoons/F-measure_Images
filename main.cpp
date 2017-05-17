d561a524c0cc7a4e39a5a4afe496e0a252021080
PARK SEONGHOON <hoonhoons.kr@gmail.com>
2017-05-17 10:38:18 +0900

    오류 수정
    오류 수정 및 일부 변경

--- main.cpp
+++ main.cpp
@@ -10,7 +10,7 @@ int main(int argc, char* argv[]) {
 	}
 
 	/*
-	F-measure_EdgeDetection.exe input_list.txt truth_list.txt
+	F-measure_EdgeDetection.exe output_list.txt truth_list.txt
 	*/
 
 	std::ifstream input_list(argv[1]);
@@ -21,8 +21,10 @@ int main(int argc, char* argv[]) {
 	int true_positive(0), true_negative(0), false_positive(0), false_negative(0);
 
 	while (!input_list.eof()) { // Assumption : len(input_list) == len(output_list)
+		int true_positive_in(0), true_negative_in(0), false_positive_in(0), false_negative_in(0);
+
 		count++;
-		
+		std::cout << "Count : " << count << std::endl;
 		std::string input_path, truth_path;
 		std::getline(input_list, input_path);
 		std::getline(truth_list, truth_path);
@@ -34,34 +36,49 @@ int main(int argc, char* argv[]) {
 		cv::threshold(truthImg, truthBinary, 127, 255, cv::THRESH_BINARY);
 
 		if (inputBinary.size() != truthBinary.size()) {
-			std::cout << "[Warning] count : " << count << ", img size is not same!\n";
+			std::cout << "[Warning] img size is not same!\n";
 			continue;
 		}
 
-		for (int j = 0; j = truthBinary.rows; ++j) {
-			for (int i = 0; i = truthBinary.cols; ++i) {
+		for (int j = 0; j < truthBinary.rows; ++j) {
+			for (int i = 0; i < truthBinary.cols; ++i) {
 				unsigned char pixel = truthImg.at<unsigned char>(j, i);
 				bool isEqual = (truthBinary.at<unsigned char>(j, i) == inputBinary.at<unsigned char>(j, i));
 				if (pixel == 255) { // white
 					if (isEqual)
-						true_positive++;
+						true_positive_in++;
 					else
-						false_negative++;
+						false_negative_in++;
 				}
 				else { // pixel == 0, black
 					if (isEqual)
-						true_negative++;
+						true_negative_in++;
 					else
-						false_positive++;
+						false_positive_in++;
 				}
 			}
+
 		}
+
+		double precision_in = (double)true_positive_in / (true_positive_in + false_positive_in);
+		double recall_in = (double)true_positive_in / (true_positive_in + false_negative_in);
+		double Fmeasure_in = 2 * precision_in * recall_in / (precision_in + recall_in);
+		std::cout << "Precision is " << precision_in << std::endl;
+		std::cout << "Recall is " << recall_in << std::endl;
+		std::cout << "F-measure is " << Fmeasure_in << std::endl << std::endl;
+
+		false_positive += false_positive_in;
+		false_negative += false_negative_in;
+		true_positive += true_positive_in;
+		true_negative += true_negative_in;
 	}
 
 	double precision = (double)true_positive / (true_positive + false_positive);
 	double recall = (double)true_positive / (true_positive + false_negative);
 	double Fmeasure = 2 * precision * recall / (precision + recall);
-	
+
+	std::cout << "****************************************" << std::endl;
+	std::cout << "OVERALL";
 	std::cout << "Precision is " << precision << std::endl;
 	std::cout << "Recall is " << recall << std::endl;
 	std::cout << "F-measure is " << Fmeasure << std::endl;
